564ca173e8e49aaba5381df02d266c2565311b4b
Wim Looman <wim@nemo157.com>
2018-12-09 17:46:39 +0100

    Fix code js

--- _includes/code.js
+++ _includes/code.js
@@ -1,9 +1,11 @@
-let me = document.currentScript
-let playground = me.previousElementSibling
-let linkContainer = playground.previousElementSibling
-let link = linkContainer.children[0]
-let snippet = linkContainer.previousElementSibling.children[0].children[0]
-link.href = `https://play.rust-lang.org/?version=nightly&edition=2018&code=${encodeURIComponent(playground.text)}`
-link.style.display = ''
-snippet.style = 'position: relative;'
-snippet.prepend(link)
+(() => {
+  let me = document.currentScript
+  let playground = me.previousElementSibling
+  let linkContainer = playground.previousElementSibling
+  let link = linkContainer.children[0]
+  let snippet = linkContainer.previousElementSibling.children[0].children[0]
+  link.href = `https://play.rust-lang.org/?version=nightly&edition=2018&code=${encodeURIComponent(playground.text)}`
+  link.style.display = ''
+  snippet.style = 'position: relative;'
+  snippet.prepend(link)
+})()
